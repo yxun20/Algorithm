4ad74498d33aca2188683d3db5fa3c31d39c0ffe
성영준 <69851018+yxun20@users.noreply.github.com>
2025-01-09 09:22:02 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] ar){
+        Scanner in= new Scanner(System.in);
+        int A =in.nextInt();
+        int B =in.nextInt();
+        
+        int C= A+B;
+        System.out.println(C);
+        in.close();
+    }
+}
