0af0509face3b4b0c2ecdce5fb5923925f808fe0
성영준 <69851018+yxun20@users.noreply.github.com>
2025-01-10 17:15:18 +0900

    [Bronze II] Title: 분산처리, Time: 1004 ms, Memory: 15684 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(bf.readLine());
+        
+        for(int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            int result = 1;
+            for(int j = 0; j < b; j++) {
+                result = (result * a) % 10;    
+            }
+            if(result == 0) {
+                System.out.println(10);
+            }else {
+                System.out.println(result);
+            }
+        }
+ 
+    }
+ 
+}
