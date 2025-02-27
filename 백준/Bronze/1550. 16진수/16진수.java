7318c45aed6d5a6086831e52e037b39bf43b8925
성영준 <69851018+yxun20@users.noreply.github.com>
2025-02-27 09:00:12 +0900

    [Bronze II] Title: 16진수, Time: 212 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.concurrent.atomic.DoubleAccumulator;
+
+public class Main{
+    
+     public static void main(String[] ar){
+         Scanner in= new Scanner(System.in);
+         String a = in.next();
+         int b=Integer.parseInt(a,16);
+         System.out.println(b);
+    }
+} 
