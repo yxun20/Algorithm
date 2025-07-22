3be0fff378593a045d873aca9f67f3523ea5a583
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-22 21:28:29 +0900

    [level 1] Title: 모의고사, Time: 5.78 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,42 +1,36 @@
 def solution(answers):
-    human1 = list(range(1,6))
+    human1 = [1,2,3,4,5]
     human2 = [2,1,2,3,2,4,2,5]
     human3 = [3,3,1,1,2,2,4,4,5,5]
-    sum = [0,0,0]
-    a = 0
-    b = 0
-    c = 0
-    q = 0
+    point = [0,0,0]
+    a, b, c = 0, 0, 0
+    answer = []
     
-    for i in range(len(answers)):
-        if human1[a] == answers[i]:
-            print(f"휴먼 1{human1[a]}{answers[i]}")
-            sum[0] = sum[0] + 1
+    for i1 in range(len(answers)):
+        if answers[i1] == human1[a]:
+            point[0] = point[0] + 1
             
-        if human2[b] == answers[i]:
-            print(f"휴먼 2{human2[b]}{answers[i]}")
-            sum[1] = sum[1] + 1
-            
-        if human3[c] == answers[i]:
-            print(f"휴먼 3{human3[c]}{answers[i]}")
-            sum[2] = sum[2] + 1
+        if answers[i1] == human2[b]:
+            point[1] = point[1] + 1
+        
+        if answers[i1] == human3[c]:
+            point[2] = point[2] + 1
             
         a = a + 1
-        if a == 5:
-            a = 0
         b = b + 1
+        c = c + 1
+        
+        if a == 5:
+            a = 0 
         if b == 8:
             b = 0
-        c = c + 1
         if c == 10:
             c = 0
-    max = 0
-    for i in range(3):
-        if max < sum[i]:
-            max = sum[i]
-    answer = []
-    for i in range(3):
-        if sum[i] == max:
-            answer.append(i+1)
-
+    
+    ans = max(point)
+    
+    for i2 in range (3):
+        if ans == point[i2]:
+            answer.append(i2+1)
+            
     return answer
