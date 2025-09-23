41ada91b3944f298a93cf14d5a8d08d59fa55dcc
성영준 <69851018+yxun20@users.noreply.github.com>
2025-09-23 21:43:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.36 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,22 +2,18 @@ def solution(participant, completion):
     
     #해시 테이블 생성
     dic ={ }
-    
-    #참가자들의 이름을 해시 테이블에 추가
     for p in participant:
         if p in dic:
             dic[p] += 1
         else:
             dic[p] = 1
-    
-    #완주한  선수들의 이름을 키로 하는 값을 1씩 감소
     for c in completion:
-        dic[c] -= 1
+        if c in dic:
+            dic[c] -= 1
     
-    #해시 테이블에 남아있는 선수가 완주하지 못한 선수
-    for key in dic.keys():
-        if dic[key] > 0:
-            return key
+    for p in participant:
+        if dic[p] > 0:
+            return p
         
-    answer = key
+    answer = p
     return answer
