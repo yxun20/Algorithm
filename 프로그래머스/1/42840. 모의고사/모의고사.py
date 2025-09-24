b1c307a60cf7bce770d203e8892a4d85da897037
성영준 <69851018+yxun20@users.noreply.github.com>
2025-09-24 12:17:58 +0900

    [level 1] Title: 모의고사, Time: 2.93 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,36 +1,35 @@
 def solution(answers):
-    human1 = [1,2,3,4,5]
-    human2 = [2,1,2,3,2,4,2,5]
-    human3 = [3,3,1,1,2,2,4,4,5,5]
-    point = [0,0,0]
-    a, b, c = 0, 0, 0
+    hum1 = [1,2,3,4,5]
+    hum2 = [2,1,2,3,2,4,2,5]
+    hum3 = [3,3,1,1,2,2,4,4,5,5]     
     answer = []
-    
-    for i1 in range(len(answers)):
-        if answers[i1] == human1[a]:
-            point[0] = point[0] + 1
-            
-        if answers[i1] == human2[b]:
-            point[1] = point[1] + 1
+    cnt1 = cnt2 = cnt3 = 0
+    a =  h2 = h3 = 0
+    for i in range (len(answers)):
         
-        if answers[i1] == human3[c]:
-            point[2] = point[2] + 1
-            
-        a = a + 1
-        b = b + 1
-        c = c + 1
-        
+        if answers[i] == hum1[a]:
+            cnt1 += 1
+        if answers[i] == hum2[h2]:
+            cnt2 += 1
+        if answers[i] == hum3[h3]:
+            cnt3 += 1
+        a += 1
+        h2 += 1
+        h3 += 1
         if a == 5:
-            a = 0 
-        if b == 8:
-            b = 0
-        if c == 10:
-            c = 0
+            a=0
+        if h2 == 8:
+            h2=0
+        if h3 == 10:
+            h3=0
     
-    ans = max(point)
+    maxx = max(cnt1, cnt2, cnt3)
     
-    for i2 in range (3):
-        if ans == point[i2]:
-            answer.append(i2+1)
-            
+    if cnt1 == maxx:
+        answer.append(1)
+    if cnt2 == maxx:
+        answer.append(2)
+    if cnt3 == maxx:
+        answer.append(3)
+    
     return answer
